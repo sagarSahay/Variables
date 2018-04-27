56b84a0eff1817aedcd26a6a64dc277ce408c63c
sagarSahay <sagar.sahay@gmail.com>
2018-04-27 19:59:02 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x= 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2; 
+
+    println!("The value of y is {}", y);
+}
